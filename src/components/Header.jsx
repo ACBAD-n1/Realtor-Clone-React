c68485865c4516d2ae540e4d73800bca1a798750
Adam Boureima <adamcbad@gmail.com>
2025-04-22 11:17:19 +0100

    Create Sign In, Sign Up, Forgot Password, Or Google Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,13 @@ function Header() {
     const location = useLocation()
     const navigate = useNavigate()
 
-    function $pathMathRoute(Route){
-        if(Route === location.pathname){
-            return true
+    console.log("Current path:", location.pathname);
+
+    function $pathMatchRoute(Route) {
+        if (Route === location.pathname) {
+            return true;
+        } else {
+            return false;
         }
     }
 
@@ -23,18 +27,18 @@ function Header() {
             <div>
                 <ul className='flex space-x-6'>
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-                            ${$pathMathRoute("/") && "text-black border-b-red-500"}`}
+                            ${$pathMatchRoute("/") ? "text-black border-b-red-500" : ""}`}
                             onClick={()=>navigate("/")}>
                         Home
                     </li>
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-                            ${$pathMathRoute("/offers") && "text-black border-b-red-500"}`}
+                            ${$pathMatchRoute("/offers") ? "text-black border-b-red-500" : ""}`}
                             onClick={()=>navigate("/offers")}>
                         Offers
                     </li>
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-                            ${$pathMathRoute("/sign-in") && "text-black border-b-red-500"}`}
-                            onClick={()=>navigate("sign-in")}>
+                            ${$pathMatchRoute("/sign-in") ? "text-black border-b-red-500" : ""}`}
+                            onClick={()=>navigate("/sign-in")}>
                         Sign in
                     </li>
                 </ul>
